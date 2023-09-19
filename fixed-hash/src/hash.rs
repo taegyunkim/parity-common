980e91c7888a908ba2af119dadc6e4311907309c
Taegyun Kim <k.taegyun@gmail.com>
2023-09-18 21:23:07 -0400

    derive proptest_derive::Arbitrary for hashes

--- fixed-hash/src/hash.rs
+++ fixed-hash/src/hash.rs
@@ -43,6 +43,7 @@ macro_rules! construct_fixed_hash {
 	( $(#[$attr:meta])* $visibility:vis struct $name:ident ( $n_bytes:expr ); ) => {
 		#[repr(C)]
 		$(#[$attr])*
+        #[cfg_attr(feature = "proptest", derive(proptest_derive::Arbitrary))]
 		$visibility struct $name (pub [u8; $n_bytes]);
 
 		impl From<[u8; $n_bytes]> for $name {
